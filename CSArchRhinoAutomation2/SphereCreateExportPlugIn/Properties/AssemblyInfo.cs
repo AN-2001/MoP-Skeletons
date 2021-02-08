d3b697639861cfce73bd70b8debf6cc10314eea7
Kacper Pluta <kacper.pluta@gmail.com>
2021-02-08 03:07:13 +0200

    init projects

---
+++ CSArchRhinoAutomation2/SphereCreateExportPlugIn/Properties/AssemblyInfo.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using Rhino.PlugIns;
+
+// Plug-in Description Attributes - all of these are optional
+// These will show in Rhino's option dialog, in the tab Plug-ins
+[assembly: PlugInDescription(DescriptionType.Address, "Technion\r\nTaub Building\r\nHaifa, 320000")]
+[assembly: PlugInDescription(DescriptionType.Country, "Israel")]
+[assembly: PlugInDescription(DescriptionType.Email, "kacper.pluta@gmail.com")]
+[assembly: PlugInDescription(DescriptionType.Phone, "")]
+[assembly: PlugInDescription(DescriptionType.Fax, "")]
+[assembly: PlugInDescription(DescriptionType.Organization, "Technion")]
+[assembly: PlugInDescription(DescriptionType.UpdateUrl, "https://github.com/copyme/CSArch-demo")]
+[assembly: PlugInDescription(DescriptionType.WebSite, "https://copyme.github.io/misc/cs-arch-spring")]
+[assembly: PlugInDescription(DescriptionType.Icon, "SphereCreateExportPlugIn.Resources.SampleCs.ico")]
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SphereCreateExportPlugIn")] // Plug-In title is extracted from this
+[assembly: AssemblyDescription("SphereCreateExportPlugIn")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Technion")]
+[assembly: AssemblyProduct("SphereCreateExportPlugIn")]
+[assembly: AssemblyCopyright("Copyright © 2021, KP")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("954da49f-0c15-4dd6-8e28-9fb1d1311c4b")] // This will also be the Guid of the Rhino plug-in
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CSArchRhinoAutomation2/SphereCreateExportPlugIn/SphereCreateExportPlugInObject.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Rhino;
+using Rhino.Geometry;
+
+namespace SphereCreateExportPlugIn
+{
+    [System.Runtime.InteropServices.ComVisible(true)]
+    public class SphereCreateExportPlugInObject
+    {
+        /// <summary>
+        /// Return a sample string
+        /// </summary>
+        public string GetName()
+        {
+            return "SphereCreateExportPlugInObject";
+        }
+
+        /// <summary>
+        /// We create a sphere centered at x, y, z of a given radius using Rhino Common API
+        /// For more info. see: https://developer.rhino3d.com/api/RhinoCommon/html/M_Rhino_Geometry_Sphere__ctor_1.htm
+        /// </summary>
+        public Rhino.Commands.Result AddSphere(double x, double y, double z, double radius)
+        {
+            var doc = RhinoDoc.ActiveDoc;
+            Rhino.Geometry.Point3d center = new Rhino.Geometry.Point3d(x, y, z);
+            Rhino.Geometry.Sphere sphere = new Rhino.Geometry.Sphere(center, radius);
+            System.Guid objG = doc.Objects.AddSphere(sphere);
+            if (objG != System.Guid.Empty)
+            {
+                doc.Views.Redraw();
+                return Rhino.Commands.Result.Success;
+            }
+            return Rhino.Commands.Result.Failure;
+        }
+
+        /// <summary>
+        /// All the objects are saved as STEP file.
+        /// For more info. see: 
+        /// https://developer.rhino3d.com/api/RhinoCommon/html/M_Rhino_FileIO_FileStp_Write.htm
+        ///  https://developer.rhino3d.com/api/RhinoCommon/html/T_Rhino_FileIO_FileStpWriteOptions.htm
+        /// </summary>
+        /// <returns></returns>
+        public Rhino.Commands.Result SaveSTP(string filepath)
+        {
+            var doc = RhinoDoc.ActiveDoc;
+            var options = new Rhino.FileIO.FileStpWriteOptions();
+            if (Rhino.FileIO.FileStp.Write(filepath, doc, options))
+            {
+                return Rhino.Commands.Result.Success;
+            }
+            else
+            {
+                return Rhino.Commands.Result.Failure;
+            }
+        }
+    }
+}
